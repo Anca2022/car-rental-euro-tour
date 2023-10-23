9e94e8be7e2633a91d9afc6507d81f37c4c752a7
Anca Matei <ancamatei2012@gmail.com>
2023-10-23 22:17:13 +0300

    car page template done

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Mobile menu
+// + TO DO: sa iasa din meniu la ESC
 let menuBtn = document.getElementsByClassName('mobile-menu')[0];
 let exitMenuBtn = document.getElementsByClassName('close-menu')[0];
 let navigation = document.getElementsByClassName('navbar')[0];
@@ -9,4 +11,17 @@ exitMenuBtn.addEventListener("click", ()=>{
     navigation.classList.remove('display-menu');
     exitMenuBtn.classList.remove('display-exit-btn')
 })
-// + sa iasa din meniu la ESC
+
+
+//language menu
+let languageBtn = document.getElementsByClassName('language')[0];
+let languageMenu = document.getElementsByClassName('other-language')[0];
+languageBtn.addEventListener('mouseover', ()=> {
+languageMenu.classList.add('display-languages');
+})
+languageBtn.addEventListener('mouseout', ()=> {
+    languageMenu.classList.remove('display-languages');
+    })
+languageBtn.addEventListener('click', ()=> {
+    languageMenu.classList.remove('display-languages');
+    })
